51fe35bcefd600bb87c8b797a0e0884968bcc5e7
Blindsp0t <contact@blindsp0t.com>
2022-07-05 20:01:30 +0200

    add slideshow

--- slider.js
+++ slider.js
@@ -1,11 +1,16 @@
-var slide = new Array("https://raw.githubusercontent.com/Ardebois/ardebois/main/images/IMG-20200912-WA0020.jpg", "https://raw.githubusercontent.com/Ardebois/ardebois/main/images/IMG-20220309-WA0001.jpg", "https://raw.githubusercontent.com/Ardebois/ardebois/main/images/IMG_20220526_180352.jpg",
+var slide = new Array("https://raw.githubusercontent.com/Ardebois/ardebois/main/images/IMG-20200912-WA0020.jpg", 
+	"https://raw.githubusercontent.com/Ardebois/ardebois/main/images/IMG-20220309-WA0001.jpg", "https://raw.githubusercontent.com/Ardebois/ardebois/main/images/IMG_20220526_180352.jpg",
 "https://raw.githubusercontent.com/Ardebois/ardebois/main/images/IMG_20211223_192153.jpg");
+
 var numero = 0;
+
 function ChangeSlide(sens) {
-numero = numero + sens;
-if (numero < 0)
-numero = slide.length - 1;
-if (numero > slide.length - 1)
-numero = 0;
-document.getElementById("slide").src = slide[numero];
+	numero = numero + sens;
+	if (numero < 0)
+		numero = slide.length - 1;
+	if (numero > slide.length - 1)
+		numero = 0;
+	document.getElementById("slide").src = slide[numero];
 }
+
+setInterval("ChangeSlide(1)", 4000);
